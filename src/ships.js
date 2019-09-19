49451044bee0f578c380b3d6b35f62591455348b
Talaa Zina <talazina@gmail.com>
2019-09-19 21:41:33 +0100

    update ships factory and tests

--- src/ships.js
+++ src/ships.js
@@ -1,21 +1,36 @@
 const Ship = (length) => {
   let health = length;
+  let positions = [];
 
-  const hit = () => {
-    health = health - 1;
-    return health;
+  const setPositions = ( posArr ) => {
+    positions = posArr.map( ( elm ) => {
+        return {cell : elm, hit : 0 };
+    });
+    return positions;
+  }
+
+  const hit = (cell) => {
+    for (var i = 0; i < positions.length; i++) {
+      if (positions[i].cell == cell) {
+        positions[i].hit = 1;   
+      }     
+    }
+    return positions;
   }
 
   const isSunk = () => {
-    if (health <= 0) {
+    let nbrHit = 0;
+    for (var i = 0; i < positions.length; i++) {
+      if (positions[i].hit) {
+        nbrHit= nbrHit + 1;   
+      } 
+    } 	
+    if (health === nbrHit) {
       return true;
     }
     return false;
-  }
+  } 
 
-  const positions = (pos) => {
-    return pos;
-  }
-  return {health, hit, isSunk, positions}
+  return {health, hit, isSunk, positions, setPositions}
 }
 module.exports = Ship;
--- src/ships.test.js
+++ src/ships.test.js
@@ -6,29 +6,36 @@ describe('Ship factory', () => {
     //expect(ship(1)).toEqual( {"health": 1, "hit": [Function hit]});
   });
 
-  it('should create a ship with one dimension', () => {
+  it('should create a ship with the dimension specified', () => {
      expect(ship(1).health).toBe(1);
+     expect(ship(2).health).toBe(2);
+     expect(ship(3).health).toBe(3);
   });
-  it('should decrease the health of the ship ', () => {
+  it('should change the status of the specified position to 1', () => {
      const ship2 = ship(2);
-     expect(ship2.hit()).toEqual(1);
-     expect(ship2.hit()).toEqual(0);
+     ship2.setPositions([4,5]);
+     expect(ship2.hit(4)).toEqual([{"cell": 4, "hit": 1}, {"cell": 5, "hit": 0}]);
+     expect(ship2.hit(5)).toEqual([{"cell": 4, "hit": 1}, {"cell": 5, "hit": 1}]);
   });
   it('should return true if the ship is sunk ', () => {
     const ship2 = ship(2);
-    ship2.hit();
-    ship2.hit();
+    ship2.setPositions([4,5]);
+    ship2.hit(4);
+    ship2.hit(5);
     expect(ship2.isSunk()).toBeTruthy()
   });
   it('should return false if the ship is not sunk ', () => {
     const ship2 = ship(2);
-    ship2.hit();
+    ship2.setPositions([4,5]);
+    ship2.hit(4);
     expect(ship2.isSunk()).toBeFalsy();
   });
   it('should return ship positions ', () => {
-    //expect(ship(1)).toEqual( {"health": 1, "hit": [Function hit]});
     const ship1 = ship(1);
-    expect(ship1.positions('pos')).toBe('pos');
+    expect(ship1.setPositions([0])).toEqual([{"cell": 0, "hit": 0}]);
+
+    const ship2 = ship(2);
+    expect(ship2.setPositions([4,5])).toEqual([{"cell": 4, "hit": 0}, {"cell": 5, "hit": 0}])
   });
 
 });
